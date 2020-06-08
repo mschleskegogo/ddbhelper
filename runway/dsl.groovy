23c8c4325a38e5d1839949699d2a5424cda8de91
Mark Schleske <mschleske@gogoair.com>
2020-06-08 13:46:56 -0600

    (init) Initial commit of runway files

---
+++ runway/dsl.groovy
@@ -0,0 +1,46 @@
+import utilities.GogoUtilities
+
+SHELL_STEPS=sprintf('''
+  npm update
+  npm install --production
+  cp -R node_modules src/
+  ''', ['JOB_NAME'])
+
+// API docs: https://jenkinsci.github.io/job-dsl-plugin/
+def myJob = job("$SRC_JOB") {
+  parameters {
+    stringParam('GIT_BUILD_BRANCH', 'master', 'Git branch used to build.')
+  }
+  logRotator {
+    numToKeep(5)
+    artifactNumToKeep(5)
+  }
+  scm {
+    git {
+      remote {
+        url("$REPO")
+        credentials('gitlab-jenkinsci')
+      }
+      branch('$GIT_BUILD_BRANCH')
+    }
+  }
+  wrappers {
+    preBuildCleanup()
+  }
+  configure { project ->
+    project / buildWrappers << 'org.jenkinsci.plugins.nvm.NvmWrapper' {
+        version('v12.18.0')
+    }
+  }
+  triggers {
+    scm('* * * * *')
+  }
+  steps {
+    shell(SHELL_STEPS)
+  }
+}
+
+g = new GogoUtilities(job: myJob)
+g.addBaseOptions(artifact_type='lambda')
+g.addSlack(slack_room='#ba-dash-eco')
+~
